d51d939c0292ca3fe853130839afa224d6e6736c
enzomelomartins <enzofullstack>
2025-04-28 15:15:43 -0300

    Criando a table produtos

--- database/migrations/2025_04_28_180909_create_produtos_table.php
+++ database/migrations/2025_04_28_180909_create_produtos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome')->unique();
+            $table->string('descricao');
+            $table->decimal('preco', 8, 2);
             $table->timestamps();
         });
     }
